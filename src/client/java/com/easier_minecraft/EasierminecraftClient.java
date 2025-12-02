85112252eb2e8ed8a37afb66b9541fc6fc3b3121
littlecym <344338668@qq.com>
2025-12-03 00:08:24 +0800

    git init

---
+++ src/client/java/com/easier_minecraft/EasierminecraftClient.java
@@ -0,0 +1,10 @@
+package com.easier_minecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EasierminecraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/easier_minecraft/EasierminecraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.easier_minecraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EasierminecraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
