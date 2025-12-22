7d9587bb5b08aa3599e230c035c0c7cdd89da4f4
littlecym <344338668@qq.com>
2025-12-22 22:44:23 +0800

    head update

--- src/client/java/com/easier_minecraft/datagen/RecipeDatagenProvider.java
+++ src/client/java/com/easier_minecraft/datagen/RecipeDatagenProvider.java
@@ -1,5 +1,6 @@
 package com.easier_minecraft.datagen;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 import com.easier_minecraft.EasierMinecraft;
@@ -11,6 +12,7 @@ import net.minecraft.data.server.recipe.CookingRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.BlastingRecipe;
 import net.minecraft.recipe.Ingredient;
@@ -21,102 +23,117 @@ import net.minecraft.registry.RegistryWrapper.WrapperLookup;
 import net.minecraft.util.Identifier;
 
 public class RecipeDatagenProvider extends FabricRecipeProvider {
+    private static final List<ItemConvertible> headsConvertibleToSkeletonSkull = List.of(
+            Items.PLAYER_HEAD,
+            Items.CREEPER_HEAD,
+            Items.ZOMBIE_HEAD,
+            Items.PIGLIN_HEAD);
 
-        public RecipeDatagenProvider(FabricDataOutput output, CompletableFuture<WrapperLookup> registriesFuture) {
-                super(output, registriesFuture);
-        }
+    public RecipeDatagenProvider(FabricDataOutput output, CompletableFuture<WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
 
-        @Override
-        public String getName() {
-                return "Recipe datagen provider";
-        }
+    @Override
+    public String getName() {
+        return "Recipe datagen provider";
+    }
 
-        @Override
-        public void generate(RecipeExporter exporter) {
-                ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, ItemRegister.EXPLOSION_BOW)
-                                .input(Items.BOW)
-                                .input(Items.TNT)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.BOW))
-                                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "explosion_bow"));
-                ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, ItemRegister.TELEPORT_BOW)
-                                .input(Items.BOW)
-                                .input(Items.ENDER_PEARL)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.BOW))
-                                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "teleport_bow"));
-                ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, ItemRegister.FIREWORK_ROCKET_BOW)
-                                .input(Items.BOW)
-                                .input(Items.FIREWORK_ROCKET)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.BOW))
-                                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "firework_rocket_bow"));
-                ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, ItemRegister.VILLAGER_BOW)
-                                .input(Items.BOW)
-                                .input(Items.EMERALD)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.BOW))
-                                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "villager_bow"));
-                ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ItemRegister.EMERALD_APPLE)
-                                .pattern("EEE")
-                                .pattern("EAE")
-                                .pattern("EEE")
-                                .input('E', Ingredient.ofItems(Items.EMERALD))
-                                .input('A', Ingredient.ofItems(Items.ENCHANTED_GOLDEN_APPLE))
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.EMERALD))
-                                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "emerald_apple"));
-                ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, Items.TOTEM_OF_UNDYING)
-                                .pattern(" E ")
-                                .pattern("EGE")
-                                .pattern(" E ")
-                                .input('E', Ingredient.ofItems(Items.EMERALD))
-                                .input('G', Ingredient.ofItems(Items.GOLD_INGOT))
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.EMERALD))
-                                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "totem_of_undying"));
-                CookingRecipeJsonBuilder
-                                .create(Ingredient.ofItems(Items.RAW_COPPER_BLOCK), RecipeCategory.BUILDING_BLOCKS,
-                                                Items.COPPER_BLOCK, 6.3F, 100, RecipeSerializer.BLASTING,
-                                                BlastingRecipe::new)
-                                .group(null)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_COPPER_BLOCK))
-                                .offerTo(exporter, getItemPath(Items.COPPER_BLOCK) + "_from_blasting_"
-                                                + getItemPath(Items.RAW_COPPER_BLOCK));
-                CookingRecipeJsonBuilder
-                                .create(Ingredient.ofItems(Items.RAW_IRON_BLOCK), RecipeCategory.BUILDING_BLOCKS,
-                                                Items.IRON_BLOCK, 6.3F, 100, RecipeSerializer.BLASTING,
-                                                BlastingRecipe::new)
-                                .group(null)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_IRON_BLOCK))
-                                .offerTo(exporter, getItemPath(Items.IRON_BLOCK) + "_from_blasting_"
-                                                + getItemPath(Items.RAW_IRON_BLOCK));
-                CookingRecipeJsonBuilder
-                                .create(Ingredient.ofItems(Items.RAW_GOLD_BLOCK), RecipeCategory.BUILDING_BLOCKS,
-                                                Items.GOLD_BLOCK, 9.0F, 100, RecipeSerializer.BLASTING,
-                                                BlastingRecipe::new)
-                                .group(null)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_GOLD_BLOCK))
-                                .offerTo(exporter, getItemPath(Items.GOLD_BLOCK) + "_from_blasting_"
-                                                + getItemPath(Items.RAW_GOLD_BLOCK));
-                CookingRecipeJsonBuilder
-                                .create(Ingredient.ofItems(Items.RAW_COPPER_BLOCK), RecipeCategory.BUILDING_BLOCKS,
-                                                Items.COPPER_BLOCK, 6.3F, 200, RecipeSerializer.SMELTING,
-                                                SmeltingRecipe::new)
-                                .group(null)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_COPPER_BLOCK))
-                                .offerTo(exporter, getItemPath(Items.COPPER_BLOCK) + "_from_smelting_"
-                                                + getItemPath(Items.RAW_COPPER_BLOCK));
-                CookingRecipeJsonBuilder
-                                .create(Ingredient.ofItems(Items.RAW_IRON_BLOCK), RecipeCategory.BUILDING_BLOCKS,
-                                                Items.IRON_BLOCK, 6.3F, 200, RecipeSerializer.SMELTING,
-                                                SmeltingRecipe::new)
-                                .group(null)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_IRON_BLOCK))
-                                .offerTo(exporter, getItemPath(Items.IRON_BLOCK) + "_from_smelting_"
-                                                + getItemPath(Items.RAW_IRON_BLOCK));
-                CookingRecipeJsonBuilder
-                                .create(Ingredient.ofItems(Items.RAW_GOLD_BLOCK), RecipeCategory.BUILDING_BLOCKS,
-                                                Items.GOLD_BLOCK, 9.0F, 200, RecipeSerializer.SMELTING,
-                                                SmeltingRecipe::new)
-                                .group(null)
-                                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_GOLD_BLOCK))
-                                .offerTo(exporter, getItemPath(Items.GOLD_BLOCK) + "_from_smelting_"
-                                                + getItemPath(Items.RAW_GOLD_BLOCK));
-        }
+    @Override
+    public void generate(RecipeExporter exporter) {
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, ItemRegister.EXPLOSION_BOW)
+                .input(Items.BOW)
+                .input(Items.TNT)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.BOW))
+                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "explosion_bow"));
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, ItemRegister.TELEPORT_BOW)
+                .input(Items.BOW)
+                .input(Items.ENDER_PEARL)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.BOW))
+                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "teleport_bow"));
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, ItemRegister.FIREWORK_ROCKET_BOW)
+                .input(Items.BOW)
+                .input(Items.FIREWORK_ROCKET)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.BOW))
+                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "firework_rocket_bow"));
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, ItemRegister.VILLAGER_BOW)
+                .input(Items.BOW)
+                .input(Items.EMERALD)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.BOW))
+                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "villager_bow"));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ItemRegister.EMERALD_APPLE)
+                .pattern("EEE")
+                .pattern("EAE")
+                .pattern("EEE")
+                .input('E', Ingredient.ofItems(Items.EMERALD))
+                .input('A', Ingredient.ofItems(Items.ENCHANTED_GOLDEN_APPLE))
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.EMERALD))
+                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "emerald_apple"));
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, Items.TOTEM_OF_UNDYING)
+                .pattern(" E ")
+                .pattern("EGE")
+                .pattern(" E ")
+                .input('E', Ingredient.ofItems(Items.EMERALD))
+                .input('G', Ingredient.ofItems(Items.GOLD_INGOT))
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.EMERALD))
+                .offerTo(exporter, Identifier.of(EasierMinecraft.MOD_ID, "totem_of_undying"));
+        CookingRecipeJsonBuilder
+                .create(Ingredient.ofItems(Items.RAW_COPPER_BLOCK), RecipeCategory.BUILDING_BLOCKS,
+                        Items.COPPER_BLOCK, 6.3F, 100, RecipeSerializer.BLASTING,
+                        BlastingRecipe::new)
+                .group(null)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_COPPER_BLOCK))
+                .offerTo(exporter, getItemPath(Items.COPPER_BLOCK) + "_from_blasting_"
+                        + getItemPath(Items.RAW_COPPER_BLOCK));
+        CookingRecipeJsonBuilder
+                .create(Ingredient.ofItems(Items.RAW_IRON_BLOCK), RecipeCategory.BUILDING_BLOCKS,
+                        Items.IRON_BLOCK, 6.3F, 100, RecipeSerializer.BLASTING,
+                        BlastingRecipe::new)
+                .group(null)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_IRON_BLOCK))
+                .offerTo(exporter, getItemPath(Items.IRON_BLOCK) + "_from_blasting_"
+                        + getItemPath(Items.RAW_IRON_BLOCK));
+        CookingRecipeJsonBuilder
+                .create(Ingredient.ofItems(Items.RAW_GOLD_BLOCK), RecipeCategory.BUILDING_BLOCKS,
+                        Items.GOLD_BLOCK, 9.0F, 100, RecipeSerializer.BLASTING,
+                        BlastingRecipe::new)
+                .group(null)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_GOLD_BLOCK))
+                .offerTo(exporter, getItemPath(Items.GOLD_BLOCK) + "_from_blasting_"
+                        + getItemPath(Items.RAW_GOLD_BLOCK));
+        CookingRecipeJsonBuilder
+                .create(Ingredient.ofItems(Items.RAW_COPPER_BLOCK), RecipeCategory.BUILDING_BLOCKS,
+                        Items.COPPER_BLOCK, 6.3F, 200, RecipeSerializer.SMELTING,
+                        SmeltingRecipe::new)
+                .group(null)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_COPPER_BLOCK))
+                .offerTo(exporter, getItemPath(Items.COPPER_BLOCK) + "_from_smelting_"
+                        + getItemPath(Items.RAW_COPPER_BLOCK));
+        CookingRecipeJsonBuilder
+                .create(Ingredient.ofItems(Items.RAW_IRON_BLOCK), RecipeCategory.BUILDING_BLOCKS,
+                        Items.IRON_BLOCK, 6.3F, 200, RecipeSerializer.SMELTING,
+                        SmeltingRecipe::new)
+                .group(null)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_IRON_BLOCK))
+                .offerTo(exporter, getItemPath(Items.IRON_BLOCK) + "_from_smelting_"
+                        + getItemPath(Items.RAW_IRON_BLOCK));
+        CookingRecipeJsonBuilder
+                .create(Ingredient.ofItems(Items.RAW_GOLD_BLOCK), RecipeCategory.BUILDING_BLOCKS,
+                        Items.GOLD_BLOCK, 9.0F, 200, RecipeSerializer.SMELTING,
+                        SmeltingRecipe::new)
+                .group(null)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.RAW_GOLD_BLOCK))
+                .offerTo(exporter, getItemPath(Items.GOLD_BLOCK) + "_from_smelting_"
+                        + getItemPath(Items.RAW_GOLD_BLOCK));
+        offerSmelting(exporter, headsConvertibleToSkeletonSkull, RecipeCategory.MISC, Items.SKELETON_SKULL, 0.7F, 200,
+                null);
+        CookingRecipeJsonBuilder
+                .create(Ingredient.ofItems(Items.SKELETON_SKULL), RecipeCategory.MISC,
+                        Items.WITHER_SKELETON_SKULL, 0.7F, 200, RecipeSerializer.SMELTING,
+                        SmeltingRecipe::new)
+                .group(null)
+                .criterion("has_item", RecipeDatagenProvider.conditionsFromItem(Items.SKELETON_SKULL))
+                .offerTo(exporter, getItemPath(Items.WITHER_SKELETON_SKULL) + "_from_smelting_"
+                        + getItemPath(Items.SKELETON_SKULL));
+    }
 
 }
--- src/main/java/com/easier_minecraft/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/com/easier_minecraft/mixin/ServerPlayerEntityMixin.java
@@ -11,13 +11,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.easier_minecraft.register.EnchantmentRegister;
 
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -32,7 +36,8 @@ public class ServerPlayerEntityMixin {
     private Optional<RegistryEntry.Reference<Enchantment>> voidSalvationEntry = null;
 
     private boolean shouldTeleport() {
-        return player.interactionManager.getGameMode() == GameMode.SURVIVAL || player.interactionManager.getGameMode() == GameMode.ADVENTURE;
+        return player.interactionManager.getGameMode() == GameMode.SURVIVAL
+                || player.interactionManager.getGameMode() == GameMode.ADVENTURE;
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
@@ -60,12 +65,30 @@ public class ServerPlayerEntityMixin {
         }
     }
 
-
     @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
     private void invulnerableToVoid(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         if (source.isOf(DamageTypes.OUT_OF_WORLD) && !shouldTeleport()) {
             cir.setReturnValue(false);
             cir.cancel();
+        }
+    }
+
+    @Inject(method = "onDeath", at = @At("HEAD"))
+    private void dropPlayerHead(DamageSource damageSource, CallbackInfo ci) {
+        if (player.isCreative() || player.isSpectator()) {
+            return;
+        }
+        ItemStack skull = new ItemStack(Items.PLAYER_HEAD);
+        ProfileComponent profileComponent = new ProfileComponent(player.getGameProfile());
+        skull.set(DataComponentTypes.PROFILE, profileComponent);
+        if (!player.getInventory().insertStack(skull)) {
+            ItemEntity playerHeadEntity = new ItemEntity(
+                    player.getWorld(),
+                    player.getX(),
+                    player.getY(),
+                    player.getZ(),
+                    skull);
+            player.getWorld().spawnEntity(playerHeadEntity);
         }
     }
 
